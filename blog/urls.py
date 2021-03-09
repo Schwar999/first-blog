1d8c3f0c789c088c023e6b3a9f4a9f17cee763f5
Schwar999 <autumnal17flush@gmail.com>
2021-03-09 17:17:15 +0900

    Change the HTML for the site 6

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.contrib import admin
+
 
 urlpatterns = [
-	path('admin/',admin.site.urls),
+    path('admin/',admin.site.urls),
     path('', include('blog.urls')),
 ]
