33be1f33c70f0d41aec205195359bf76dc1b2975
Schwar999 <autumnal17flush@gmail.com>
2021-03-09 17:13:37 +0900

    Change the HTML for the site 4

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+	path('admin/',admin.site.urls),
+    path('', include('blog.urls')),
 ]
